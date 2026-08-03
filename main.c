68735c4fa27ff3855e8b5e1afbc95f8edc238c24
nikov-afk <kallash.nniikov@gmail.com>
2026-08-03 15:03:17 -0300

    Aula 197

--- main.c
+++ main.c
@@ -1,26 +1,18 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void maior_menor(int vetor[], int tam, int *menor, int *maior) {
-  *maior = vetor[0];
-  *menor = vetor[0];
-  for (int i = 0; i < tam; i++) {
-    if (vetor[i] > *maior) {
-      *maior = vetor[i];
-    }
-    if (vetor[i] < *menor) {
-      *menor = vetor[i];
-    }
-  }
-  printf("maior numero: %d\n",*maior);
-  printf("menor numero: %d\n",*menor);
-}
+// Alocação dinâmica de memória com a função MALLOC | Aula 197
 
 int main() {
-  int menor = 0, maior = 0, vetor[] = {110, 265, 30, 410, 544, 684, 74, 880, 95};
-  maior_menor(vetor, 9, &menor, &maior);
-  printf("Menor valor: %d\n", menor);
-  printf("Maior valor: %d\n", maior);
-  return 0;
+    int *x;
+    x = malloc(sizeof(int));
+    if (x) {
+        printf("malloc ok\n");
+        printf("X: %d\n",*x);
+        *x = 1;
+        printf("X: %d\n",*x);
+    }else {
+        printf("malloc failed\n");
+    }
+    return 0;
 }
