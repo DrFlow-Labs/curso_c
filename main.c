4aa8fe574ba0254e9e82e128724b06c977e1034a
nikov-afk <kallash.nniikov@gmail.com>
2026-08-04 10:06:17 -0300

    Aula 199

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Alocação dinâmica de memória com a função CALLOC | Aula 198
+// Como alocar um vetor dinâmico na linguagem C?  | Aula 199
 
 int main() {
-    int *x;
-    x = calloc(1,sizeof(int));
-    if (x) {
-        printf("malloc ok\n");
-        printf("X: %d\n",*x);
-        *x = 1;
-        printf("X: %d\n",*x);
+    int tam, *vet;
+    srand(time(NULL));
+    printf("Digite a tamanho do vetor: ");
+    scanf("%d", &tam);
+    vet = (int *) malloc(tam * sizeof(int));
+    if (vet) {
+        for (int i = 0; i < tam; i++) {
+            vet[i] = rand() % 10;
+        }
+        for (int i = 0; i < tam; i++) {
+            printf("%d ", vet[i]);
+        }
     }else {
-        printf("malloc failed\n");
+        printf("Erro ao alocar o memoria!");
     }
+
     return 0;
 }
