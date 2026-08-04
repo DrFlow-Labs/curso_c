9e02f7df35a816416fc96c2f9c3b4ae9b78eafc3
nikov-afk <kallash.nniikov@gmail.com>
2026-08-04 15:21:34 -0300

    Aula 200

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Como alocar um vetor dinâmico na linguagem C?  | Aula 199
+/*
+    Aula 200: Alocação dinâmica de memória com a função realloc.
+    -> Retorna um ponteiro para a nova região de memória.
+    -> Se o ponteiro for nulo ela aloca memória.
+    -> Se o novo tamanho for zero a memória é liberada.
+    -> Se não houver memória suficiente retorna null.
+*/
 
 int main() {
     int tam, *vet;
@@ -15,6 +21,13 @@ int main() {
             vet[i] = rand() % 10;
         }
         for (int i = 0; i < tam; i++) {
+            printf("%d ", vet[i]);
+        }
+        printf("\nDigite o novo tamanho do vetor: ");
+        scanf("%d", &tam);
+        vet = (int *) realloc(vet, tam * sizeof(int));
+        printf("Vetor realocado :\n");
+       for (int i = 0; i < tam; i++) {
             printf("%d ", vet[i]);
         }
     }else {
