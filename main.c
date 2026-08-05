98db90bbe03320a700491b66863c4e8b9bfffebc
nikov-afk <kallash.nniikov@gmail.com>
2026-08-05 12:12:18 -0300

    Aula 204

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
-        Aula 202: Como alocar uma matriz dinâmica?
-
-        vetor de vetores -> int* -> 10 27 32
-                            int* -> 11 75 49
-                            int* -> 43 82 10
-                            int* -> 12 47 62
-*/
+// Como percorrer uma matriz dinâmica com aritmética de ponteiros? Aula 204
 int main() {
     int **mat,i,j;
     srand(time(NULL));
@@ -19,17 +12,20 @@ int main() {
     }
     for(i=0;i<4;i++) {
         for(j=0;j<4;j++) {
-            mat[i][j] = rand() % 100;
+            //mat[i][j] = rand() % 100;
+            *(*(mat + i) + j) = rand() % 100;
         }
     }
     for(i=0;i<4;i++) {
         for(j=0;j<4;j++) {
-            printf("%d ",mat[i][j]);
+            //printf("%d ",mat[i][j]);
+            printf("%d",*(*(mat + i) + j));
         }
         printf("\n");
     }
     for(i=0;i<4;i++) {
-        free(mat[i]);
+        //free(mat[i]);
+        free(*(mat + i));
     }
     free(mat);
     return 0;
