3baf23a0e0ee146f5f8348a9dfa2abce1f997990
nikov-afk <kallash.nniikov@gmail.com>
2026-08-05 10:57:52 -0300

    Aula 202

--- main.c
+++ main.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-// como liberar memória alocada dinamicamente com a função free()? Aula 201
+/*
+        Aula 202: Como alocar uma matriz dinâmica?
 
+        vetor de vetores -> int* -> 10 27 32
+                            int* -> 11 75 49
+                            int* -> 43 82 10
+                            int* -> 12 47 62
+*/
 int main() {
-    int tam, *vet;
+    int **mat,i,j;
     srand(time(NULL));
-    printf("Digite a tamanho do vetor: ");
-    scanf("%d", &tam);
-    vet = (int *) malloc(tam * sizeof(int));
-    if (vet) {
-        for (int i = 0; i < tam; i++) {
-            vet[i] = rand() % 10;
-        }
-        for (int i = 0; i < tam; i++) {
-            printf("%d ", vet[i]);
-        }
-        printf("\nDigite o novo tamanho do vetor: ");
-        scanf("%d", &tam);
-        vet = (int *) realloc(vet, tam * sizeof(int));
-        printf("Vetor realocado :\n");
-        for (int i = 0; i < tam; i++) {
-            printf("%d ", vet[i]);
-        }
-        free(vet);
-    }else {
-        printf("Erro ao alocar o memoria!");
+    mat = malloc(4 * sizeof(int*));
+    for(i=0;i<4;i++) {
+        mat[i] = malloc(4 * sizeof(int));
     }
-
+    for(i=0;i<4;i++) {
+        for(j=0;j<4;j++) {
+            mat[i][j] = rand() % 100;
+        }
+    }
+    for(i=0;i<4;i++) {
+        for(j=0;j<4;j++) {
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<4;i++) {
+        free(mat[i]);
+    }
+    free(mat);
     return 0;
 }
